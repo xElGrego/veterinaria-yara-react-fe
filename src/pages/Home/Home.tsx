bae481d3c5c80a3e91d1400655b6582e19be20de
Gregorio Avila <gregorioavilam@hotmail.com>
2023-09-04 23:28:24 -0500

    dashboard

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react";
 import { useAppSelector } from "../../store/store";
 import { nombreSelector } from "../../redux/User/user.selector";
+import { Header } from "./Components/Header";
 
 export const Home: FC = () => {
   const nombre = useAppSelector(nombreSelector);
 
   return (
-    <div>
-      Dashboard
-      <div>Nombre : {nombre}</div>
+    <div className="flex gap-6">
+      <Header />
+      <div className="m3 text-xl text-gray-900 font-semibold">Router</div>
     </div>
   );
 };
