a4325f8c828c4478e9cc7557f01690acc66a7137
Gregorio Avila <gregorioavilam@hotmail.com>
2023-09-30 12:32:28 -0500

    Fix table

--- src/pages/Mascotas/Components/MascotasList.tsx
+++ src/pages/Mascotas/Components/MascotasList.tsx
@@ -29,7 +29,7 @@ export const MascotasList: FC = () => {
     name: string;
   }
 
-  const data: Item[] = [
+  /* const data: Item[] = [
     {
       id: 1,
       title: "Ant Design Title 1",
@@ -50,26 +50,26 @@ export const MascotasList: FC = () => {
       title: "Ant Design Title 4",
       name: "nam4",
     },
-  ];
+  ]; */
 
   const [checked, setChecked] = useState<string[]>([]);
   const [indeterminate, setIndeterminate] = useState<boolean>(false);
   const [checkAll, setCheckAll] = useState<boolean>(false);
 
   useEffect(() => {
-    setIndeterminate(checked.length > 0 && checked.length !== data.length);
-    setCheckAll(checked.length === data.length);
+    setIndeterminate(checked.length > 0 && checked.length !== Mascotas.length);
+    setCheckAll(checked.length === Mascotas.length);
   }, [checked]);
 
   const onCheckAllChange = (e: CheckboxChangeEvent) => {
-    setChecked(e.target.checked ? data.map((item) => item.title) : []);
+    setChecked(e.target.checked ? Mascotas.map((item) => item.idMascota) : []);
     setCheckAll(e.target.checked);
   };
 
   return (
     <div className="px-4 sm:px-6 lg:px-8 my-2 text-xs w-auto">
       <div className=" flex flex-col ">
-        <div>
+        {/* <div>
           <Checkbox
             indeterminate={indeterminate}
             onChange={onCheckAllChange}
@@ -86,16 +86,12 @@ export const MascotasList: FC = () => {
           >
             <List
               itemLayout="vertical"
-              dataSource={data}
-              renderItem={(item: Item) => (
+              dataSource={Mascotas}
+              renderItem={(item: IMascota) => (
                 <List.Item>
                   <List.Item.Meta
-                    avatar={<Checkbox value={item.title} />}
-                    title={
-                      <a href="https://ant.design">
-                        {item.title} - {item.id}{" "}
-                      </a>
-                    }
+                    avatar={<Checkbox value={item.idMascota} />}
+                    title={<a href="https://ant.design">{item.nombre}</a>}
                     description="1"
                   />
                 </List.Item>
@@ -105,74 +101,86 @@ export const MascotasList: FC = () => {
           <div style={{ marginTop: 20 }}>
             <b>Selecting:</b> {checked.join(", ")}
           </div>
-        </div>
+        </div> */}
         <div className="-my-2 -mx-4 sm:-mx-6 lg:-mx-8">
           <div className=" min-w-full py-2 align-middle">
             <div className="ring-1 ring-black    max-h-[65vh] overflow-x-auto w-auto  ring-opacity-5 md:rounded-lg  shadow bg-white dark:bg-gray-900  ">
-              <table className="min-w-full table-fixed   divide-y w-auto divide-gray-300 dark:divide-gray-700 ">
-                {Mascotas.length == 0 ? (
+              <table className="min-w-full table-fixed divide-y w-auto divide-gray-300 dark:divide-gray-700">
+                {Mascotas.length === 0 ? (
                   <span></span>
                 ) : (
-                  <thead className="text-center divide-y top-0 z-50 bg-white divide-gray-200 dark:divide-gray-700 dark:bg-gray-800 dark:text-gray-400">
-                    <tr className="divide-x divide-gray-50 ">
-                      <th scope="col" className="py-3.5 pl-4 pr-3 sm:pl-6">
-                        Nombre
-                      </th>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell hidden"
-                      >
-                        Apodo
-                      </th>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell hidden"
-                      >
-                        Edad
-                      </th>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell"
-                      >
-                        Peso
-                      </th>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell"
-                      >
-                        Raza
-                      </th>
-                      <th
-                        scope="col"
-                        className="py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell"
-                      >
-                        Fecha Registro
-                      </th>
-
-                      <th scope="col" className="py-3.5 pl-4 pr-3 sm:pl-6  ">
-                        Opciones
-                      </th>
-                    </tr>
-                  </thead>
-                )}
-
-                <tbody className="text-center divide-y  text-white">
-                  {!IsLoading ? (
-                    Mascotas.length > 0 ? (
-                      Mascotas.map((el: IMascota, key: number) => (
+                  <>
+                    <thead className="text-center divide-y top-0 z-50 bg-white divide-gray-200 dark:divide-gray-700 dark:bg-gray-800 dark:text-gray-400">
+                      <tr className="divide-x divide-gray-50">
+                        <th>
+                          <Checkbox
+                            indeterminate={indeterminate}
+                            onChange={onCheckAllChange}
+                            checked={checkAll}
+                          ></Checkbox>
+                        </th>
+                        <th scope="col" className="py-3.5 pl-4 pr-3 sm:pl-6">
+                          Nombre
+                        </th>
+                        <th
+                          scope="col"
+                          className="py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell hidden"
+                        >
+                          Apodo
+                        </th>
+                        <th
+                          scope="col"
+                          className="py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell hidden"
+                        >
+                          Edad
+                        </th>
+                        <th
+                          scope="col"
+                          className="py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell"
+                        >
+                          Peso
+                        </th>
+                        <th
+                          scope="col"
+                          className="py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell"
+                        >
+                          Raza
+                        </th>
+                        <th
+                          scope="col"
+                          className="py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell"
+                        >
+                          Fecha Registro
+                        </th>
+                        <th scope="col" className="py-3.5 pl-4 pr-3 sm:pl-6">
+                          Opciones
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody className="text-center divide-y text-white">
+                      {Mascotas.map((el: IMascota, key: number) => (
                         <tr key={key}>
+                          <td>
+                            <Checkbox.Group
+                              style={{ width: "100%" }}
+                              value={checked}
+                              onChange={(checkedValues) => {
+                                setChecked(checkedValues as string[]);
+                              }}
+                            >
+                              <Checkbox value={el.idMascota} />
+                            </Checkbox.Group>
+                          </td>
+
                           <td className="text-white py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell hidden">
                             {el.nombre}
                           </td>
-
                           <td className="text-white py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell hidden">
                             {el.mote}
                           </td>
-
                           <td className="text-white py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell hidden">
                             {el.edad}
                           </td>
-
                           <td className="text-white py-3.5 pl-4 pr-3 sm:pl-6 lg:table-cell hidden">
                             {el.peso}
                           </td>
@@ -184,27 +192,14 @@ export const MascotasList: FC = () => {
                               "DD-MM-YYYY HH:mm:ss"
                             )}
                           </td>
-
-                          <td className="py-3.5 pl-4 pr-3 sm:pl-6 ">
+                          <td className="py-3.5 pl-4 pr-3 sm:pl-6">
                             <DropDownMascota field={el} />
                           </td>
                         </tr>
-                      ))
-                    ) : (
-                      <tr>
-                        <td colSpan={8} className="py-4 text-sm">
-                          No hay datos.
-                        </td>
-                      </tr>
-                    )
-                  ) : (
-                    <tr>
-                      <td colSpan={8} className="py-4 text-sm">
-                        <Spinner class="h-6 w-6 text-blue-800" /> Cargando...
-                      </td>
-                    </tr>
-                  )}
-                </tbody>
+                      ))}
+                    </tbody>
+                  </>
+                )}
               </table>
             </div>
             {Mascotas.length > 0 && (
