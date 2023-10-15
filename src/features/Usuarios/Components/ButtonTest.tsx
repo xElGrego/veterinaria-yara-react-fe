9ebdab648b3c957727a903f7c93118f4a49cfb65
Gregorio Avila <gregorioavilam@hotmail.com>
2023-10-14 20:53:21 -0500

    withAutorizationButton

---
+++ src/features/Usuarios/Components/ButtonTest.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import { withPermissionCheck } from "../../../shared/HOC/Autorizaation/withPermisionButton";
+
+interface UsuariosFormProps {
+  onClick: () => void;
+}
+
+const ButtoPermision: FC<UsuariosFormProps> = (props) => {
+  return (
+    <>
+      <div className=" mt-4 flex justify-between gap-4">
+        <div>
+          <button
+            type="button"
+            onClick={props.onClick}
+            className="inline-block rounded bg-neutral-800 px-4 pb-2 pt-2.5 text-xs font-normal text-neutral-50 shadow-[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-neutral-800 hover:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]"
+          >
+            Agregar
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const ButtoPermisionWithPermissions =
+  withPermissionCheck(ButtoPermision);
